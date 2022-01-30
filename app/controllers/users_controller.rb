4a59d399652e48f6244f01f641ca8718ca4a556e
shinozakikenichi <ee.ihcinek@icloud.com>
2022-01-30 18:24:16 +0900

    ユーザー編集機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,13 @@ class UsersController < ApplicationController
 
   def edit
   end
-  
+
   def update
-    current_user.update(user_params)
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
   private
